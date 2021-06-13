0d0ac3e2bbf5781485b91c5250f25541fc810f8e
Ashe-w <ashley.w.cwc@gmail.com>
2021-06-13 14:11:52 -0700

    12345

--- app/src/main/java/com/example/appproject/FirstFragment.java
+++ app/src/main/java/com/example/appproject/FirstFragment.java
@@ -4,20 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import org.w3c.dom.Text;
+
 public class FirstFragment extends Fragment {
+    TextView countTextView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        View firstLayOut=inflater.inflate(R.layout.fragment_first, container, false);
+        countTextView = firstLayOut.findViewById(R.id.D);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return firstLayOut;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -30,5 +37,36 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+        //buttonA
+        view.findViewById(R.id.A).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast toastA = Toast.makeText(getActivity(),"show toast A",Toast.LENGTH_SHORT);
+                toastA.show();
+            }
+        });
+        view.findViewById(R.id.B).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCount(v);
+            }
+        });
+        view.findViewById(R.id.C).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                randomNum(v);
+            }
+        });
+    }
+    public void addCount(View view){
+
+        String textD = countTextView.getText().toString();
+        Integer count=Integer.parseInt(textD);
+        count++;
+        countTextView.setText(count.toString());
+    }
+    public void randomNum(View view){
+        Integer number = (int)(Math.random()*100);
+        countTextView.setText(number.toString());
     }
 }
--- app/src/main/java/com/example/appproject/MainActivity.java
+++ app/src/main/java/com/example/appproject/MainActivity.java
@@ -22,14 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -44,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        //comment
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
