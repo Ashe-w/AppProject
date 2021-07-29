fd781a1358bee478d59596c8d676643c9b979bca
Ashe-w <ashley.w.cwc@gmail.com>
2021-07-28 17:06:09 -0700

    some changes

--- app/src/main/java/com/example/appproject/FirstFragment.java
+++ app/src/main/java/com/example/appproject/FirstFragment.java
@@ -4,69 +4,58 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import org.w3c.dom.Text;
 
-public class FirstFragment extends Fragment {
-    TextView countTextView;
+import java.util.ArrayList;
 
+public class FirstFragment extends Fragment {
+
+    ArrayList<String> arrayList=new ArrayList<>();
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        View firstLayOut=inflater.inflate(R.layout.fragment_first, container, false);
-        countTextView = firstLayOut.findViewById(R.id.D);
-        // Inflate the layout for this fragment
+
+        View firstLayOut = inflater.inflate(R.layout.fragment_first, container, false);
+        ListView listView=firstLayOut.findViewById(R.id.listview);
+
+        arrayList.add("apple");
+        arrayList.add("banana");
+        arrayList.add("cat");
+        arrayList.add("dog");
+        ArrayAdapter adapter1=new ArrayAdapter(firstLayOut.getContext(),android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(adapter1);
+
+
         return firstLayOut;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        Button b2 = view.findViewById(R.id.button2);
+
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                arrayList.add("n");
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
-        //buttonA
-        view.findViewById(R.id.A).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Toast toastA = Toast.makeText(getActivity(),"show toast A",Toast.LENGTH_SHORT);
-                toastA.show();
-            }
-        });
-        view.findViewById(R.id.B).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addCount(v);
-            }
-        });
-        view.findViewById(R.id.C).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                randomNum(v);
-            }
-        });
-    }
-    public void addCount(View view){
-
-        String textD = countTextView.getText().toString();
-        Integer count=Integer.parseInt(textD);
-        count++;
-        countTextView.setText(count.toString());
-    }
-    public void randomNum(View view){
-        Integer number = (int)(Math.random()*100);
-        countTextView.setText(number.toString());
     }
 }
--- app/src/main/java/com/example/appproject/MainActivity.java
+++ app/src/main/java/com/example/appproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appproject;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,44 +8,43 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,new FirstFragment(),"First Fragment").commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        //comment
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
